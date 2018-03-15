91f8b344d0e379effa50c6cc27b33d2e47abec35
Israelem <israelem@gmail.com>
2018-03-15 14:18:21 +0100

    Add files via upload

---
+++ buscaminas.py
@@ -0,0 +1,86 @@
+import random as rd
+
+
+def calcular_minas():
+    return 10
+
+
+def calcular_adyacentes(t_interno, fila, columna):
+    if t_interno[fila][columna] != -1:
+        minas_adyacentes = 0
+        for d_fila in range(-1, 2):
+            for d_columna in range(-1, 2):
+                if 0 <= fila + d_fila < len(t_interno) and \
+                        0 <= columna + d_columna < len(t_interno) and \
+                        t_interno[fila + d_fila][columna + d_columna] == -1:
+                    minas_adyacentes += 1
+        t_interno[fila][columna] = minas_adyacentes
+
+
+def generar_tablero_interno(tamaño, minas):
+    # Inicializado a cero
+    t_interno = [[0*x for x in range(tamaño)] for x in range(tamaño)]
+    pares = [(x, y) for x in range(tamaño) for y in range(len(t_interno))]
+    rd.shuffle(pares)
+    for par in pares[:minas]:
+        # La mina se representa con un -1
+        t_interno[par[0]][par[1]] = -1
+    for fila in range(len(t_interno)):
+        for columna in range(len(t_interno)):
+            calcular_adyacentes(t_interno, fila, columna)
+
+    return t_interno
+
+
+def inicializar_tablero_externo(tamaño):
+    return [['.' for x in range(tamaño)] for x in range(tamaño)]
+
+
+def finaliza_juego(minas, cubiertas):  # , t_externo):
+    respuesta = False
+    if cubiertas == minas:
+        respuesta = True
+    return respuesta
+
+
+def mostrar_datos(t_externo):
+    for fila in t_externo:
+        print(fila)
+
+
+def leer_movimiento():
+    fila, columna = [int(x) for x in input('Escribe fila y columna a descubrir: ').split()]
+    return [fila, columna]
+
+
+def calcular_movimiento():
+    pass
+
+
+def descubrir_casilla(t_interno, t_externo, fila, columna):
+    for d_fila in range(-1, 2):
+        for d_columna in range(-1, 2):
+            if 0 <= fila + d_fila < len(t_externo) and \
+                    0 <= columna + d_columna < len(t_externo):
+                if t_interno[fila + d_fila][columna + d_columna] == 0:
+                    t_externo[fila + d_fila][columna + d_columna] = ' '
+                    t_interno[fila + d_fila][columna + d_columna] = -2
+                    descubrir_casilla(t_interno, t_externo, fila + d_fila, columna + d_columna)
+                elif t_interno[fila + d_fila][columna + d_columna] > 0:
+                    t_externo[fila + d_fila][columna + d_columna] = t_interno[fila + d_fila][columna + d_columna]
+
+
+if __name__ == '__main__':
+
+    TAMAÑO = 8
+
+    minas = calcular_minas()
+    t_interno = generar_tablero_interno(TAMAÑO, minas)
+    cubiertas = len(t_interno) ** 2
+    t_externo = inicializar_tablero_externo(len(t_interno))
+    fin_juego = False
+    while finaliza_juego(minas, cubiertas):
+        mostrar_datos(t_externo)
+        coordenadas = leer_movimiento()
+        descubrir_casilla(t_interno, t_externo, coordenadas[0], coordenadas[1])
+        # calcular_movimiento()
