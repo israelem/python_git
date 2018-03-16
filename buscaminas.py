cba8ab94bd403d76ad7bc759659904f563618086
Israelem <israelem@gmail.com>
2018-03-16 09:50:49 +0100

    Update buscaminas.py

--- buscaminas.py
+++ buscaminas.py
@@ -19,7 +19,7 @@ def calcular_adyacentes(t_interno, fila, columna):
 
 def generar_tablero_interno(tamaño, minas):
     # Inicializado a cero
-    t_interno = [[0*x for x in range(tamaño)] for x in range(tamaño)]
+    t_interno = [[0 for x in range(tamaño)] for x in range(tamaño)]
     pares = [(x, y) for x in range(tamaño) for y in range(len(t_interno))]
     rd.shuffle(pares)
     for par in pares[:minas]:
@@ -28,7 +28,6 @@ def generar_tablero_interno(tamaño, minas):
     for fila in range(len(t_interno)):
         for columna in range(len(t_interno)):
             calcular_adyacentes(t_interno, fila, columna)
-
     return t_interno
 
 
@@ -36,16 +35,18 @@ def inicializar_tablero_externo(tamaño):
     return [['.' for x in range(tamaño)] for x in range(tamaño)]
 
 
-def finaliza_juego(minas, cubiertas):  # , t_externo):
-    respuesta = False
-    if cubiertas == minas:
-        respuesta = True
-    return respuesta
-
-
 def mostrar_datos(t_externo):
-    for fila in t_externo:
-        print(fila)
+    print()
+    print('  ', end='')
+    for numero in range(len(t_externo)):
+        print(numero, end='')
+    print()
+    for fila in enumerate(t_externo):
+        print(fila[0], end=' ')
+        for casilla in fila[1]:
+            print(casilla, end='')
+        print()
+    print()
 
 
 def descubrir_casilla(t_interno, t_externo, fila, columna):
@@ -60,16 +61,28 @@ def descubrir_casilla(t_interno, t_externo, fila, columna):
                 elif t_interno[fila + d_fila][columna + d_columna] > 0:
                     t_externo[fila + d_fila][columna + d_columna] = t_interno[fila + d_fila][columna + d_columna]
 
-        
+
 def calcular_movimiento(t_interno, t_externo):
+    respuesta = False
     instrucción, fila, columna = [x for x in input('Introduzca código [d/m/?] fila y columna a descubrir: ').split()]
+    fila, columna = int(fila), int(columna)
     if instrucción == 'd':
         if t_interno[fila][columna] != -1:
             descubrir_casilla(t_interno, t_externo, fila, columna)
+        else:
+            respuesta = True
     elif instrucción == 'm':
         t_externo[fila][columna] = 'm'
     elif instrucción == '?':
         t_externo[fila][columna] = '?'
+    return respuesta
+
+
+def calcular_cubieras(t_interno):
+    cubiertas = 0
+    for fila in t_interno:
+        cubiertas += fila.count(-2)
+    return cubiertas
 
 
 if __name__ == '__main__':
@@ -78,9 +91,12 @@ if __name__ == '__main__':
 
     minas = calcular_minas()
     t_interno = generar_tablero_interno(TAMAÑO, minas)
-    cubiertas = len(t_interno) ** 2
     t_externo = inicializar_tablero_externo(len(t_interno))
-    fin_juego = False
-    while finaliza_juego(minas, cubiertas):
+    bomba = False
+    while not bomba and minas != calcular_cubieras(t_interno):
         mostrar_datos(t_externo)
-        calcular_movimiento(t_interno, t_externo)
+        bomba = calcular_movimiento(t_interno, t_externo)
+    if bomba:
+        print('Lo siento, has perdido')
+    else:
+        print('¡Enhorabuena! Has ganado')
